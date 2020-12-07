3d1b40dfb15fa04dd3ffab0a45d433a59d2baad6
zieunx <wldms_1217@naver.com>
2020-12-08 00:17:19 +0900

    설계개선하기 - 자율성을 높이자(Audience 캡슐화)

--- chapter1/example1/src/main/java/com/study/objects/Objects/model/TicketSeller.java
+++ chapter1/example1/src/main/java/com/study/objects/Objects/model/TicketSeller.java
@@ -10,15 +10,8 @@ public class TicketSeller {
 
     // getTicketOffice() 메소드 제거
 
-    // TicketOffice 에 대한 접근은 오직 TicketSeller에서만 하도록 수정되었다.
+    // TicketSeller가 Audience의 인터페이스에만 의존하도록 수정했다.
     public void sellTo(Audience audience) {
-        if (audience.getBag().hasInvitation()) {
-            Ticket ticket = ticketOffice.getTicket();
-        } else {
-            Ticket ticket = ticketOffice.getTicket();
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketOffice.plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
+        ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()));
     }
 }
