bb11812200645ebbe9a100b1657696a35842b680
zieunx <wldms_1217@naver.com>
2022-02-20 23:13:59 +0900

    chapter1과 chapter2 프로젝트 제거

--- chapter1/example1/src/main/java/com/study/objects/Objects/model/Invitation.java
+++
@@ -1,8 +0,0 @@
-package com.study.objects.Objects.model;
-
-import java.time.LocalDateTime;
-
-// 초대장
-public class Invitation {
-    private LocalDateTime when;
-}
--- chapter2/example1/src/main/java/com/study/objects/Objects/model/Movie.java
+++
@@ -1,9 +0,0 @@
-package com.study.objects.Objects.model;
-
-public class Movie {
-    private int fee;
-
-    public int getFee() {
-        return fee;
-    }
-}
